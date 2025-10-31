cf5aebe82081d801ac99e2b838e6fd3023c359ec
Ally4wada <ally.awada@sptech.school>
2025-10-31 13:25:47 -0300

    Finalizado

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,28 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura {
+
+    private Double raio;
+
+    public Circulo(String cor, Integer espessura) {
+        super(cor, espessura);
+    }
+
+    public Circulo() {
+        super("branco", 12);
+    }
+
+    @Override
+    public Double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
+
 }
--- src/main/java/school/sptech/Quadrado.java
+++ src/main/java/school/sptech/Quadrado.java
@@ -1,4 +1,34 @@
 package school.sptech;
 
-public class Quadrado {
+
+public class Quadrado extends Figura {
+    private Double lado;
+
+    public Quadrado(String cor, Integer espessura) {
+        super(cor, espessura);
+    }
+
+    public Quadrado() {
+        super("Vermelho", 15);
+    }
+
+    @Override
+    public Double calcularArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Quadrado{" +
+                "lado=" + lado +
+                '}';
+    }
+
+    public Double getLado() {
+        return lado;
+    }
+
+    public void setLado(Double lado) {
+        this.lado = lado;
+    }
 }
